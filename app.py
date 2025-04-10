921012389e83af602ce0f0ef74f38bd9ed251055
Bing Bhakdibhumi <barameeb@andrew.cmu.edu>
2025-04-09 21:02:28 -0400

    changed app.run port to 5001 for testing purposes

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def translator():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
